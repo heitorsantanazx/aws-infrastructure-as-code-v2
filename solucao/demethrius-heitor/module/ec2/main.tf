54e2ebc66c46a86b85389566175809241fbb9b12
Deméthrius Heitor <heitordemethrius@gmail.com>
2026-04-01 15:40:28 -0300

    "novas alterações no projeto"

--- solucao/demethrius-heitor/module/ec2/main.tf
+++ solucao/demethrius-heitor/module/ec2/main.tf
@@ -1,13 +1,18 @@
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.selected.id
+  ami                    = data.aws_ami.amazon_linux_2023.id
   instance_type          = var.instance_type
   vpc_security_group_ids = var.security_group_ids
-  user_data              = var.user_data
+  
+  user_data = file("userdata.sh")
 
   iam_instance_profile   = var.use_instance_profile ? var.instance_profile : null
 
   tags = {
     Name        = var.instance_name
     Environment = var.environment
+  }
+
+  root_block_device {
+    volume_size = 8
   }
 }
